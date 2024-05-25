4ae36320aefdf5daeda6f16b8705985b2fb5ea18
Dominik Bartnik <dominikjanbartnik01@gmail.com>
2024-05-25 17:55:37 +0200

    fix: workflow

--- frontend/doncards/vite.config.ts
+++ frontend/doncards/vite.config.ts
@@ -3,8 +3,6 @@ import path from 'node:path'
 import electron from 'vite-plugin-electron/simple'
 import react from '@vitejs/plugin-react'
 
-const isElectron = process.env.MODE === 'electron'
-console.log(process.env.MODE);
 // MODE=electron npm run build
 
 // https://vitejs.dev/config/
@@ -30,5 +28,5 @@ export default defineConfig({
         : {},
     }),
   ],
-  ...(process.env.MODE === 'electron' ? {} : { base: '/Doncards/frontend/doncards/' })
+  ...(process.env.MODE === 'electron' ? {} : { base: '/Doncards/' })
 })
