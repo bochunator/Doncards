8ffc2f815c6bc514cf943d7f3098bd6787bac89e
Dominik Bartnik <dominikjanbartnik01@gmail.com>
2024-05-25 18:18:52 +0200

    feat: react router

--- frontend/doncards/src/App.tsx
+++ frontend/doncards/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/electron-vite.animate.svg'
 import './App.css'
+import { Link, Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <nav>
+        <Link to="/Doncards/">Home</Link>
+        {" | "}
+        <Link to="/Doncards/contact">Contact</Link>
+      </nav>
+      <Outlet />
     </>
   )
 }
--- frontend/doncards/src/main.tsx
+++ frontend/doncards/src/main.tsx
@@ -2,10 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.tsx'
+import Contact from './pages/Contact.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/Doncards/",
+    element: <App/>,
+    children: [
+      {
+        path: "/Doncards/",
+        element: <Home/>
+      },
+      {
+        path: "/Doncards/contact",
+        element: <Contact/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
 
